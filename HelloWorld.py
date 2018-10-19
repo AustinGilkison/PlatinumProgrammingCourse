5c7dafd09cf98c62f17d4e3c035a768d331b573f
AustinGilkison <34859170+austingilkison@users.noreply.github.com>
2018-10-18 22:05:46 -0400

    Created and added to HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,49 @@
+print('Learning how to print messages with the print function.')
+
+'''
+ Printing 'Hello World' to the terminal.
+'''
+print('Hello World')
+'''
+ Simply using the `print()` function we are able to print anything we need to the terminal.
+'''
+
+
+''' 
+ Printing 'Hello World' to the terminal using a variable to hold the string value of "Hello World".
+'''
+message = 'Hello World'
+print(message)
+'''
+ By assigning the variable `message` the value 'Hello World' we are able to pass `message` to the `print()` to print 
+its value 
+
+ For more information on variable and how they work and are used have a look into the `Var.py` file.
+'''
+
+# Todo Create var.py
+
+'''
+ Printing by passing a string into a custom print function called `print_me()`.
+'''
+
+
+def print_me(print_message):
+    """
+     Prints the value of the print_message var.
+    :param print_message: Message to be printed.
+    """
+    print(print_message)
+
+
+print_me('Hello World')
+
+'''
+ We call the `print_me()` function and pass it the variable or data that we would like to print, in this case being
+ 'Hello World'. Once this function has been called it then calls the `Print()` function the in turn prints our message
+ or data.
+ 
+ For more information on Functions please see the `Func.py` file.
+ For more information on Doc strings please see the `Doc.py` file.
+'''
+# Todo Create Func.py and Doc.py
